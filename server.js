4cfda9cf55c77cd38bc6a306c82342a47f36ddf2
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-05-15 17:10:57 +0500

    elon create code full

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import adminRouter from "./router/admin_router.js";
+import elonRouter from "./router/elon_router.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRouter);
+app.use("/elon", elonRouter);
 
 app.listen(PORT, () => {
   console.log(PORT);
