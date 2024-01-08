1b7c13ed748be80664e9f7856e7a3592d8babb98
shakhruz <ismoilovshaxruz775@gmail.com>
2024-01-08 16:26:58 +0500

    hello world

--- config/db_config.js
+++ config/db_config.js
@@ -17,7 +17,7 @@
 const { Sequelize, DataTypes, UUIDV4 } = require("sequelize");
 require('dotenv/config')
 
-const sequelize = new Sequelize(process.env.ELEPHANTSQLURL);
+const sequelize = new Sequelize(process.env.URL);
 sequelize
   .authenticate()
   .then(() => console.log("Connected"))
